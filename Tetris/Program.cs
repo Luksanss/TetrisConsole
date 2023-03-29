c110b0f2fa02c0abcf70afc5089887240cd1696a
Lukas <lukaspapariga@gmail.com>
2023-03-29 16:19:32 +0200

    init
    damn

---
+++ Tetris/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Tetris;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            DrawBoard();
+        }
+    }
+
+    static void DrawBoard()
+    {
+        Console.WriteLine("hez");
+    }
+}
